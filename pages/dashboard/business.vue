ee28163d0acc45bdc4a99f00c9ddf57c7d0acaf2
Femi Omoniyi <ignitor1996@gmail.com>
2021-08-16 09:25:53 +0100

    Live date

--- pages/dashboard/business.vue
+++ pages/dashboard/business.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="container">
     <c-heading marginBottom="20px">Business</c-heading>
-
-    <c-stat marginBottom="20px">
-      <c-stat-label>Total Registered Businesses</c-stat-label>
-      <c-stat-number>380,610</c-stat-number>
-      <c-stat-helper-text>
-        <c-stat-arrow type="increase" />
-        30.60%
-      </c-stat-helper-text>
-    </c-stat>
-
+     <c-stat marginBottom="20px">
+        <c-stat-label>Total Registered Businesses</c-stat-label>
+        <c-stat-number>{{counts.company_aggregate ? counts.company_aggregate.aggregate.count : 0}}</c-stat-number>
+        <c-stat-helper-text>
+          <c-stat-arrow type="increase" />
+          0.0%
+        </c-stat-helper-text>
+      </c-stat>
     <div>
-        
       <c-stack :spacing="5">
         <c-box :p="5" border-width="1px">
-            <c-grid template-columns="repeat(6, 1fr)">
+            <template v-for="business in businesses" >
+                <c-grid template-columns="repeat(6, 1fr)" :key="business.id">
                 <c-box>
                     <c-text fontSize="11px" color="gray.500">
                         Company name
                     </c-text>
                     <c-text fontSize="13px">
-                        B&D
+                       {{business.name}}
                     </c-text>
                 </c-box>
                 <c-box>
@@ -29,7 +27,7 @@
                         Stage
                     </c-text>
                     <c-text fontSize="13px">
-                        Ideation stage
+                       {{business.business_stage.name}}
                     </c-text>
                 </c-box>
                 <c-box>
@@ -37,7 +35,7 @@
                         Objective
                     </c-text>
                     <c-text fontSize="13px">
-                        Conceptualise my business idea
+                       {{business.business_objective.description}}
                     </c-text>
                 </c-box>
                   <c-box>
@@ -45,7 +43,7 @@
                         Location
                     </c-text>
                     <c-text fontSize="13px">
-                        Petran, Albania
+                       {{business.city}}, {{business.country}}
                     </c-text>
                 </c-box>
                  <c-box>
@@ -53,7 +51,7 @@
                         Investment Ready
                     </c-text>
                     <c-text fontSize="13px">
-                        4 Months
+                        {{business.investmentEtaValue}} {{business.investmentEtaMetric}}
                     </c-text>
                 </c-box>
                  <c-box>
@@ -61,64 +59,14 @@
                         Size
                     </c-text>
                     <c-text fontSize="13px">
-                        3-6
+                        {{business.size}}
                     </c-text>
                 </c-box>
                
             </c-grid>
-            <c-divider>
+            <c-divider :key="business.id">
             </c-divider>
-            <c-grid template-columns="repeat(6, 1fr)">
-                <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Company name
-                    </c-text>
-                    <c-text fontSize="13px">
-                        B&D
-                    </c-text>
-                </c-box>
-                <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Stage
-                    </c-text>
-                    <c-text fontSize="13px">
-                        Ideation stage
-                    </c-text>
-                </c-box>
-                <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Objective
-                    </c-text>
-                    <c-text fontSize="13px">
-                        Conceptualise my business idea
-                    </c-text>
-                </c-box>
-                  <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Location
-                    </c-text>
-                    <c-text fontSize="13px">
-                        Petran, Albania
-                    </c-text>
-                </c-box>
-                 <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Investment Ready
-                    </c-text>
-                    <c-text fontSize="13px">
-                        4 Months
-                    </c-text>
-                </c-box>
-                 <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Size
-                    </c-text>
-                    <c-text fontSize="13px">
-                        3-6
-                    </c-text>
-                </c-box>
-               
-            </c-grid>
+            </template>
         </c-box>
       </c-stack>
     </div>
@@ -126,69 +74,10 @@
 </template>
 
 <script lang="js">
-import { CStack } from '@chakra-ui/vue'
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    scopedSlots: { customRender: 'name' },
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-    width: 80,
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address 1',
-    ellipsis: true,
-  },
-  {
-    title: 'Long Column Long Column Long Column',
-    dataIndex: 'address',
-    key: 'address 2',
-    ellipsis: true,
-  },
-  {
-    title: 'Long Column Long Column',
-    dataIndex: 'address',
-    key: 'address 3',
-    ellipsis: true,
-  },
-  {
-    title: 'Long Column',
-    dataIndex: 'address',
-    key: 'address 4',
-    ellipsis: true,
-  },
-];
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park, New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 2 Lake Park, London No. 2 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park, Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+import countQuery from "~/graphql/queries/counts.gql";
+import businessQuery from "~/graphql/queries/listBusiness.gql";
+
 
 export default {
   name: 'App',
@@ -196,13 +85,33 @@ export default {
   layout: 'dashboard',
   data () {
       return {
-      data,
-      columns,
+         counts : {} ,
+         businesses : []
     }
+  },
+  fetch(){
+    this.getCounts();
+    this.getBusiness();
   },
   computed: {
   },
   methods: {
+    getCounts(){
+      this.$apollo.query({query : countQuery})
+        .then(({ data }) => {
+          // do what you want with data
+          console.log(data);
+          this.counts = data
+        })
+    },
+     getBusiness(){
+      this.$apollo.query({query : businessQuery})
+        .then(({ data }) => {
+          // do what you want with data
+          console.log(data);
+          this.businesses = data.company
+        })
+    }
   }
 }
 </script>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="container">
+    <c-heading marginBottom="20px">Statistics</c-heading>
+
     <c-stat-group>
       <c-stat>
         <c-stat-label>Total Registered Businesses</c-stat-label>
-        <c-stat-number>380,610</c-stat-number>
+        <c-stat-number>{{counts.company_aggregate ? counts.company_aggregate.aggregate.count : 0}}</c-stat-number>
         <c-stat-helper-text>
           <c-stat-arrow type="increase" />
-          30.60%
+          0.0%
         </c-stat-helper-text>
       </c-stat>
       <c-stat>
         <c-stat-label>Total Register Users</c-stat-label>
-        <c-stat-number>45</c-stat-number>
+        <c-stat-number>{{ counts.user_aggregate ? counts.user_aggregate.aggregate.count : 0}}</c-stat-number>
         <c-stat-helper-text>
           <c-stat-arrow type="increase" />
-          -5.20%
+          0.0%
         </c-stat-helper-text>
       </c-stat>
       <c-stat>
         <c-stat-label>Total Posts on Insights</c-stat-label>
-        <c-stat-number>45</c-stat-number>
+        <c-stat-number>{{ counts.post_aggregate ? counts.post_aggregate.aggregate.count : 0}}</c-stat-number>
         <c-stat-helper-text>
           <c-stat-arrow type="increase" />
-          -5.20%
+          0.0%
         </c-stat-helper-text>
       </c-stat>
       <c-stat>
         <c-stat-label>Total Comments on Insights</c-stat-label>
-        <c-stat-number>45</c-stat-number>
+        <c-stat-number>{{ counts.post_comment_aggregate ? counts.post_comment_aggregate.aggregate.count : 0}}</c-stat-number>
         <c-stat-helper-text>
           <c-stat-arrow type="increase" />
-          -5.20%
+          0.0%
         </c-stat-helper-text>
       </c-stat>
     </c-stat-group>
@@ -39,86 +41,98 @@
 </template>
 
 <script lang="js">
+
+import countQuery from "~/graphql/queries/counts.gql";
+
 export default {
   name: 'App',
   components: {},
   layout: 'dashboard',
   data () {
-      return {
-        chartOptions : {
+        return {
+      chartOptions: {
 
-    title: {
-        text: 'Growth'
-    },
+          title: {
+              text: 'Growth'
+          },
 
-    subtitle: {
-        text: 'Business, User, Post and Comment Growoth MoM'
-    },
+          subtitle: {
+              text: 'Business, User, Post and Comment Growoth MoM'
+          },
 
-    yAxis: {
-        title: {
-            text: 'Units'
-        }
-    },
+          yAxis: {
+              title: {
+                  text: 'Units'
+              }
+          },
 
-    xAxis: {
-        accessibility: {
-            rangeDescription: 'Jan - Dec'
-        }
-    },
+          xAxis: {
+              accessibility: {
+                  rangeDescription: 'Jan - Dec'
+              }
+          },
 
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-    },
+          legend: {
+              layout: 'vertical',
+              align: 'right',
+              verticalAlign: 'middle'
+          },
 
-    plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
-            },
-            pointStart: 2014
-        }
-    },
+          plotOptions: {
+              series: {
+                  label: {
+                      connectorAllowed: false
+                  },
+                  pointStart: 2014
+              }
+          },
 
-    series: [{
-        name: 'Business',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-    }, {
-        name: 'Users',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    }, {
-        name: 'Posts',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-    }, {
-        name: 'Comments',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-    }],
+          series: [{
+              name: 'Business',
+              data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+          }, {
+              name: 'Users',
+              data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+          }, {
+              name: 'Posts',
+              data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+          }, {
+              name: 'Comments',
+              data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+          }],
 
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                }
-            }
-        }]
-    }
-
-}
-      }
+          responsive: {
+              rules: [{
+                  condition: {
+                      maxWidth: 500
+                  },
+                  chartOptions: {
+                      legend: {
+                          layout: 'horizontal',
+                          align: 'center',
+                          verticalAlign: 'bottom'
+                      }
+                  }
+              }]
+          }
+      },
+      counts : {}
+  }
   },
+  fetch(){
+    this.getCounts();
+  },
+
   computed: {
   },
   methods: {
     getCounts(){
-      
+      this.$apollo.query({query : countQuery})
+        .then(({ data }) => {
+          // do what you want with data
+          console.log(data);
+          this.counts = data
+        })
     }
   }
 }
