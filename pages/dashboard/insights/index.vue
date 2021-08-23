7d6cd342b925c2d3d8f8b5deb4deb553877d0be4
Femi Omoniyi <ignitor1996@gmail.com>
2021-08-23 09:49:54 +0100

    Insight Page

---
+++ pages/dashboard/insights/index.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="container">
+    <c-heading marginBottom="20px">Posts</c-heading>
+    <c-stat-group>
+      <c-stat>
+        <c-stat-label>Total Posts in Insights</c-stat-label>
+        <c-stat-number>{{
+          counts.post_aggregate ? counts.post_aggregate.aggregate.count : 0
+        }}</c-stat-number>
+        <c-stat-helper-text>
+          <c-stat-arrow type="increase" />
+          0.0%
+        </c-stat-helper-text>
+      </c-stat>
+    </c-stat-group>
+ <c-tabs>
+    <c-tab-list>
+      <c-tab>Posts</c-tab>
+      <c-tab>Comments</c-tab>
+    </c-tab-list>
+    <c-tab-panels>
+      <c-tab-panel>
+         <c-box :p="5" border-width="1px">
+      <template v-for="user in users">
+        <nuxt-link :to="'./user/' + user.id" :key="user.id">
+          <c-grid template-columns="100px repeat(4, 1fr)">
+            <c-box>
+              <c-avatar
+                :name="
+                  user.firstname ? `${user.firstname} ${user.lastname}` : null
+                "
+              />
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500"> Name </c-text>
+              <c-text fontSize="13px">
+                {{
+                  user.firstname
+                    ? `${user.firstname} ${user.lastname}`
+                    : `No name`
+                }}
+              </c-text>
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500"> Email </c-text>
+              <c-text fontSize="13px">
+                {{ user.email }}
+              </c-text>
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500"> Companies </c-text>
+              <c-text fontSize="13px">
+                {{ user.teams.length ? "" : "Pending Invites" }}
+                <ul>
+                  <li v-for="team in user.teams" :key="team.id">
+                    {{ team.company.name }}
+                  </li>
+                </ul>
+              </c-text>
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500">
+                Registeration Date
+              </c-text>
+              <c-text fontSize="13px">
+                {{ $moment(user.createdAt).calendar() }}
+              </c-text>
+            </c-box>
+          </c-grid>
+        </nuxt-link>
+
+        <c-divider :key="user.id"> </c-divider>
+      </template>
+    </c-box>
+      </c-tab-panel>
+      <c-tab-panel>
+         <c-box :p="5" border-width="1px">
+      <template v-for="user in users">
+        <nuxt-link :to="'./user/' + user.id" :key="user.id">
+          <c-grid template-columns="100px repeat(4, 1fr)">
+            <c-box>
+              <c-avatar
+                :name="
+                  user.firstname ? `${user.firstname} ${user.lastname}` : null
+                "
+              />
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500"> Name </c-text>
+              <c-text fontSize="13px">
+                {{
+                  user.firstname
+                    ? `${user.firstname} ${user.lastname}`
+                    : `No name`
+                }}
+              </c-text>
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500"> Email </c-text>
+              <c-text fontSize="13px">
+                {{ user.email }}
+              </c-text>
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500"> Companies </c-text>
+              <c-text fontSize="13px">
+                {{ user.teams.length ? "" : "Pending Invites" }}
+                <ul>
+                  <li v-for="team in user.teams" :key="team.id">
+                    {{ team.company.name }}
+                  </li>
+                </ul>
+              </c-text>
+            </c-box>
+            <c-box>
+              <c-text fontSize="11px" color="gray.500">
+                Registeration Date
+              </c-text>
+              <c-text fontSize="13px">
+                {{ $moment(user.createdAt).calendar() }}
+              </c-text>
+            </c-box>
+          </c-grid>
+        </nuxt-link>
+
+        <c-divider :key="user.id"> </c-divider>
+      </template>
+    </c-box>
+      </c-tab-panel>
+    </c-tab-panels>
+  </c-tabs>
+  </div>
+</template>
+
+<script lang="js">
+
+import countQuery from "~/graphql/queries/counts.gql";
+import userQuery from "~/graphql/queries/users.gql";
+
+export default {
+  name: 'App',
+  components: {},
+  layout: 'dashboard',
+  data () {
+    return {
+      counts : {},
+      users : []
+    }
+  },
+  fetch(){
+    this.getCounts();
+    this.getUsers();
+  },
+  computed: {
+  },
+  methods: {
+    getCounts(){
+      this.$apollo.query({query : countQuery})
+        .then(({ data }) => {
+          // do what you want with data
+          console.log(data);
+          this.counts = data
+        })
+    },
+     getUsers(){
+      this.$apollo.query({query : userQuery})
+        .then(({ data }) => {
+          // do what you want with data
+          console.log(data);
+          this.users = data.user
+        })
+    }
+  }
+}
+</script>
