1834c799af50837d3c32ac7a1952abfcd6546d29
Femi Omoniyi <ignitor1996@gmail.com>
2021-08-30 09:23:49 +0100

    Post, Comment and User Mutations

--- pages/dashboard/insights/index.vue
+++ pages/dashboard/insights/index.vue
@@ -7,71 +7,146 @@
         <c-stat-number>{{
           counts.post_aggregate ? counts.post_aggregate.aggregate.count : 0
         }}</c-stat-number>
-        
       </c-stat>
-       <c-stat>
+      <c-stat>
         <c-stat-label>Total Comments in Insights</c-stat-label>
         <c-stat-number>{{
-          counts.post_comment_aggregate ? counts.post_comment_aggregate.aggregate.count : 0
+          counts.post_comment_aggregate
+            ? counts.post_comment_aggregate.aggregate.count
+            : 0
         }}</c-stat-number>
-          
       </c-stat>
-        <c-stat>
+      <c-stat>
         <c-stat-label>Total Tags in Insights</c-stat-label>
         <c-stat-number>{{
-          counts.post_comment_aggregate ? counts.post_comment_aggregate.aggregate.count : 0
+          counts.post_comment_aggregate
+            ? counts.post_comment_aggregate.aggregate.count
+            : 0
         }}</c-stat-number>
-          
       </c-stat>
     </c-stat-group>
- <c-tabs>
-    <c-tab-list>
-      <c-tab>Posts</c-tab>
-      <c-tab>Comments</c-tab>
-      <c-tab>Tags</c-tab>
-    </c-tab-list>
-    <c-tab-panels>
-      <c-tab-panel>
-         <c-box :p="5" border-width="1px">
-           <c-stack v-for="(post, index) in posts" :key="index">
-                    <c-flex>
-                    <c-avatar size="xs" v-bind:name="post.user ? `${post.user.firstname} ${post.user.lastname}` : null" />
-                    <c-text>
-                      {{post.user.firstname}} 
-                      {{post.user.lastname}}   
-                    </c-text>     
-                    </c-flex>
-                    <c-text fontSize="12px" p="0 2px" v-html="post.content">
-                    </c-text>   
-                    <c-divider></c-divider>
-            </c-stack> 
-    </c-box>
-      </c-tab-panel>
-      <c-tab-panel>
-         <c-box :p="5" border-width="1px">
-      <c-stack v-for="(comment, index) in comments" :key="index">
-                    <c-flex>
-                    <c-avatar size="xs" v-bind:name="comment.user ? `${comment.user.firstname} ${comment.user.lastname}` : null" />
-                    <c-text>
-                      {{comment.user.firstname}} 
-                      {{comment.user.lastname}}   
-                    </c-text>     
-                    </c-flex>
-                    <c-text fontSize="12px" p="0 2px" v-html="comment.content">
-                    </c-text>   
-                    <c-divider></c-divider>
-            </c-stack> 
-    </c-box>
-      </c-tab-panel>
-      <c-tab-panel>
-         <c-box :p="5" border-width="1px">
-     <c-badge v-for="(tag, index) in tags" :key="index"  mx="2" variant="subtle" variant-color="blue">{{tag.name}} - {{tag.post_tags_aggregate.aggregate.count}}</c-badge>
-    </c-box>
-      </c-tab-panel>
-    </c-tab-panels>
-  </c-tabs>
+    <c-tabs>
+      <c-tab-list>
+        <c-tab>Posts</c-tab>
+        <c-tab>Comments</c-tab>
+        <c-tab>Tags</c-tab>
+      </c-tab-list>
+      <c-tab-panels>
+        <c-tab-panel>
+          <c-flex v-if="loading" justify="center" padding="50px">
+            <c-spinner />
+          </c-flex>
+          <c-box :p="5" border-width="1px">
+            <c-stack v-for="(post, index) in posts" :key="index">
+              <c-flex>
+                <c-avatar
+                  size="xs"
+                  v-bind:name="
+                    post.user
+                      ? `${post.user.firstname} ${post.user.lastname}`
+                      : null
+                  "
+                />
+                <c-text>
+                  {{ post.user.firstname }}
+                  {{ post.user.lastname }}
+                  <c-badge mx="2" v-if="post.isDeleted" variant-color="red"
+                    >deleted</c-badge
+                  >
+                  <c-badge mx="2" v-if="post.isPromoted" variant-color="green"
+                    >promoted</c-badge
+                  >
+                </c-text>
+
+                <c-menu>
+                  <c-menu-button
+                    :aria-controls="index"
+                    class="actions"
+                    right-icon="chevron-down"
+                  >
+                    Actions
+                  </c-menu-button>
+                  <c-menu-list :id="index">
+                    <c-menu-item @click="deletePost(post.id)"
+                      >Delete</c-menu-item
+                    >
+                    <c-menu-item @click="promotePost(post.id)"
+                      >Promote</c-menu-item
+                    >
+                  </c-menu-list>
+                </c-menu>
+              </c-flex>
+              <c-text fontSize="12px" p="0 2px" v-html="post.content"> </c-text>
+              <c-divider></c-divider>
+            </c-stack>
+          </c-box>
+        </c-tab-panel>
+        <c-tab-panel>
+          <c-box :p="5" border-width="1px">
+            <c-stack v-for="(comment, index) in comments" :key="index">
+              <c-flex>
+                <c-avatar
+                  size="xs"
+                  v-bind:name="
+                    comment.user
+                      ? `${comment.user.firstname} ${comment.user.lastname}`
+                      : null
+                  "
+                />
+                <c-text>
+                  {{ comment.user.firstname }}
+                  {{ comment.user.lastname }}
+                  <c-badge mx="2" v-if="comment.isDeleted" variant-color="red"
+                    >deleted</c-badge
+                  >
+                </c-text>
+                <c-menu>
+                  <c-menu-button
+                    :aria-controls="index"
+                    class="actions"
+                    right-icon="chevron-down"
+                  >
+                    Actions
+                  </c-menu-button>
+                  <c-menu-list :id="index">
+                    <c-menu-item @click="deleteComment(comment.id)"
+                      >Delete</c-menu-item
+                    >
+                  </c-menu-list>
+                </c-menu>
+              </c-flex>
+              <c-text fontSize="12px" p="0 2px" v-html="comment.content">
+              </c-text>
+              <c-divider></c-divider>
+            </c-stack>
+          </c-box>
+        </c-tab-panel>
+        <c-tab-panel>
+          <c-box :p="5" border-width="1px">
+            <c-badge
+              v-for="(tag, index) in tags"
+              :key="index"
+              mx="2"
+              variant="subtle"
+              variant-color="blue"
+              >{{ tag.name }} -
+              {{ tag.post_tags_aggregate.aggregate.count }}</c-badge
+            >
+          </c-box>
+        </c-tab-panel>
+      </c-tab-panels>
+    </c-tabs>
   </div>
 </template>
+
+<style scoped>
+.actions {
+  padding: 3px 3px 3px 3px !important;
+  font-size: x-small;
+  height: initial;
+  margin-left: 20px;
+}
+</style>
 
 <script lang="js">
 
@@ -79,6 +154,9 @@ import countQuery from "~/graphql/queries/counts.gql";
 import postsQuery from "~/graphql/queries/posts.gql";
 import commentsQuery from "~/graphql/queries/comments.gql";
 import tagsQuery from "~/graphql/queries/tags.gql";
+import deletePost from "~/graphql/mutations/deletePost.gql";
+import promotePost from "~/graphql/mutations/promotePost.gql";
+import deleteComment from "~/graphql/mutations/deleteComment.gql";
 
 export default {
   name: 'App',
@@ -89,7 +167,8 @@ export default {
       counts : {},
       posts : [],
       comments : [],
-      tags : []
+      tags : [],
+      loading : false
     }
   },
   fetch(){
@@ -108,9 +187,11 @@ export default {
         })
     },
      getPosts(){
+      this.loading = true;
       this.$apollo.query({query : postsQuery})
         .then(({ data }) => {
           this.posts = data.post
+          this.loading = false;
         })
     },
     getComments(){
@@ -125,6 +206,30 @@ export default {
           this.tags = data.tag
         })
     },
+    deletePost(id){
+      // this.loading = true;
+      this.$apollo.mutate({mutation : deletePost, variables : {id}})
+        .then(({data})=>{
+          this.loading = false;
+          this.getPosts();
+        })
+    },
+    deleteComment(id){
+      // this.loading = true;
+      this.$apollo.mutate({mutation : deleteComment, variables : {id}})
+        .then(({data})=>{
+          this.loading = false;
+          this.getComments();
+        })
+    },
+    promotePost(id){
+      // this.loading = true;
+      this.$apollo.mutate({mutation : promotePost, variables : {id}})
+        .then(({data})=>{
+          this.loading = false;
+          this.getPosts();
+        })
+    }
   }
 }
 </script>
--- pages/dashboard/user/index.vue
+++ pages/dashboard/user/index.vue
@@ -1,68 +1,95 @@
 <template>
   <div class="container">
     <c-heading marginBottom="20px">Users</c-heading>
-<c-stat-group>
+    <c-stat-group>
       <c-stat>
         <c-stat-label>Total Register Users</c-stat-label>
-        <c-stat-number>{{ counts.user_aggregate ? counts.user_aggregate.aggregate.count : 0}}</c-stat-number>
+        <c-stat-number>{{
+          counts.user_aggregate ? counts.user_aggregate.aggregate.count : 0
+        }}</c-stat-number>
         <c-stat-helper-text>
           <c-stat-arrow type="increase" />
           0.0%
         </c-stat-helper-text>
       </c-stat>
-</c-stat-group>
+    </c-stat-group>
     <div>
-        
       <c-stack :spacing="5">
         <c-box :p="5" border-width="1px">
-          <template v-for="user in users">
-            <nuxt-link :to="'./user/'+user.id" :key="user.id">
-               <c-grid template-columns="100px repeat(4, 1fr)">
-                <c-box>
-                <c-avatar :name="user.firstname ? `${user.firstname} ${user.lastname}` : null" />
-                </c-box>
-                <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Name
-                    </c-text>
-                    <c-text fontSize="13px">
-                        {{user.firstname ? `${user.firstname} ${user.lastname}` : `No name`}}
-                    </c-text>
-                </c-box>
-                <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Email
-                    </c-text>
-                    <c-text fontSize="13px">
-                        {{user.email}}
-                    </c-text>
-                </c-box>
-                  <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Companies
-                    </c-text>
-                    <c-text fontSize="13px">
-                      {{user.teams.length ? '' : 'Pending Invites'}}
-                        <ul>
-                          <li v-for="team in user.teams" :key="team.id">
-                            {{team.company.name}}
-                          </li>
-                        </ul>
-                    </c-text>
-                </c-box>
-                 <c-box>
-                    <c-text fontSize="11px" color="gray.500">
-                        Registeration Date
-                    </c-text>
-                    <c-text fontSize="13px">
-                        {{$moment(user.createdAt).calendar()}}
-                    </c-text>
-                </c-box>       
+          <template v-for="(user, index) in users">
+            <c-grid template-columns="100px repeat(4, 1fr) 100px" :key="index">
+              <c-box>
+                <c-avatar
+                  :name="
+                    user.firstname ? `${user.firstname} ${user.lastname}` : null
+                  "
+                />
+              </c-box>
+              <c-box>
+                <c-text fontSize="11px" color="gray.500"> Name </c-text>
+                <c-text fontSize="13px">
+                  {{
+                    user.firstname
+                      ? `${user.firstname} ${user.lastname}`
+                      : `No name`
+                  }}
+                   <c-badge mx="2" v-if="user.isBlocked" variant-color="yellow"
+                    >blocked</c-badge
+                  >
+                  <c-badge mx="2" v-if="user.isDeleted" variant-color="red"
+                    >deleted</c-badge
+                  >
+                 
+                </c-text>
+              </c-box>
+              <c-box>
+                <c-text fontSize="11px" color="gray.500"> Email </c-text>
+                <c-text fontSize="13px">
+                  {{ user.email }}
+                </c-text>
+              </c-box>
+              <c-box>
+                <c-text fontSize="11px" color="gray.500"> Companies </c-text>
+                <c-text fontSize="13px">
+                  {{ user.teams.length ? "" : "Pending Invites" }}
+                  <ul>
+                    <li v-for="(team, index) in user.teams" :key="index">
+                      {{ team.company.name }}
+                    </li>
+                  </ul>
+                </c-text>
+              </c-box>
+              <c-box>
+                <c-text fontSize="11px" color="gray.500">
+                  Registeration Date
+                </c-text>
+                <c-text fontSize="13px">
+                  {{ $moment(user.createdAt).calendar() }}
+                </c-text>
+              </c-box>
+              <c-menu>
+                <c-menu-button
+                  :aria-controls="index"
+                  class="actions"
+                  right-icon="chevron-down"
+                >
+                  Actions
+                </c-menu-button>
+                <c-menu-list :id="index">
+                  <c-menu-item as="nuxt-link" :to="'./user/' + user.id"
+                    >View</c-menu-item
+                  >
+                  <c-menu-item @click="toggleBlock(user.id, user.isBlocked)">{{
+                    user.isBlocked ? "Unblock" : "Block"
+                  }}</c-menu-item>
+                  <c-menu-item @click="toggleDelete(user.id, user.isDeleted)">{{
+                    user.isDeleted ? "Undelete" : "Delete"
+                  }}</c-menu-item>
+                </c-menu-list>
+              </c-menu>
             </c-grid>
-            </nuxt-link>
-           
-            <c-divider :key="user.id">
-            </c-divider>
+
+            <c-divider :key="index"> </c-divider>
           </template>
         </c-box>
       </c-stack>
@@ -74,6 +101,8 @@
 
 import countQuery from "~/graphql/queries/counts.gql";
 import userQuery from "~/graphql/queries/users.gql";
+import toggleBlockUser from "~/graphql/mutations/toggleBlockUser.gql";
+import toggleDeleteUser from "~/graphql/mutations/toggleDeleteUser.gql";
 
 export default {
   name: 'App',
@@ -88,8 +117,6 @@ export default {
   fetch(){
     this.getCounts();
     this.getUsers();
-  },
-  computed: {
   },
   methods: {
     getCounts(){
@@ -107,7 +134,21 @@ export default {
           console.log(data);
           this.users = data.user
         })
-    }
+    },
+    toggleBlock(id,isBlocked){
+    const block = !isBlocked;
+    this.$apollo.mutate({mutation : toggleBlockUser, variables : {id, isBlocked : block}})
+        .then(({data})=>{
+          this.getUsers();
+        })
+  },
+  toggleDelete(id,isDeleted){
+    const deleted = !isDeleted;
+    this.$apollo.mutate({mutation : toggleDeleteUser, variables : {id, isDeleted : deleted}})
+        .then(({data})=>{
+          this.getUsers();
+        })
+  }
   }
 }
 </script>
